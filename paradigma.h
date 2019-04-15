4c344a95ea877e05b8ca278e52ff1e0088797727
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2019-04-15 15:36:37 -0300

    Update paradigma.h

--- paradigma.h
+++ paradigma.h
@@ -1,6 +1,9 @@
+#include "entradaSaida.h"
 #ifndef PARADIGMA
 #define PARADIGMA
 
-int forcaBruta(int i, int j, int **mat, int *clock, int maior, int tamanhoMatriz);
+int forcaBruta(int **mat, int tamanhoMatriz, Ponto* inicial);
+int submatrizValida(int i, int j, int **mat, int *clock, int maior, int tamanhoMatriz);
+int guloso(int **mat, int tamanhoMatriz, Ponto* inicial);
 
 #endif
