cee06ac1c99cb43ca1be78853586b664ce72cdee
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2019-04-15 15:36:50 -0300

    Update entradaSaida.h

--- entradaSaida.h
+++ entradaSaida.h
@@ -18,6 +18,10 @@ int calculaTamanhoMatriz(FILE* arq);
 int** alocaMatriz(int tamanhoMatriz);
 int** leituraArqEntrada(FILE* arq, int tamanhoMatriz);
 void imprimeArqSaida(FILE* arq, int** mat, int maior, Ponto inicial, int tamanhoMatriz);
+void contaTempoProcessador(double *utime, double *stime);
+void imprimeTempo(double user_time, double system_time, FILE* arq);
 void liberaPonteiros(Arquivos *arq, int** mat, int tamanhoMatriz);
+int checkSubMatrizForcaBruta(int **mat, int lin, int col, int range);
+int checkSubMatrizGuloso(int **mat, int y, int x, int maior);
 
 #endif
